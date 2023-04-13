551cda44bc3a88fd839c8bc6e107f1ce76f45097
olivier.dufour62@yahoo.com <olivier_dufour@ens.univ-artois.fr>
2023-04-14 00:51:28 +0200

    add hash password

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const Users = require("../models/userModel");
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
 exports.createUser = async (req, res, err) => {
+    let salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(req.body.user_password, salt);
   const newUser = new Users({
     user_role: req.body.user_role,
     user_name: req.body.user_name,
     user_firstname: req.body.user_firstname,
     user_mail: req.body.user_mail,
-    user_password: req.body.user_password,
+    user_password: hash,
     user_tel: req.body.user_tel,
   });
 
@@ -51,26 +54,26 @@ exports.getOneUser = async (req, res) => {
   }
 };
 
-// exports.updateRead = async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     if (!ObjectId.isValid(id)) {
-//       return res.status(404).json({ message: "Invalid User id" });
-//     }
+exports.updateUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+      return res.status(404).json({ message: "Invalid User id" });
+    }
 
-//     const User = await User.findById(id);
+    const User = await User.findById(id);
 
-//     if (!User) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     res.status(200).json(User);
+    if (!User) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(User);
 
-//     await User.findByIdAndUpdate(id, {
-//       read: true,
-//       date_read: Date.now(),
-//     });
+    await User.findByIdAndUpdate(id, {
+      read: true,
+      date_read: Date.now(),
+    });
   
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
